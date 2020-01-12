54969456351f5b6e0f83c495cdc8d2e5a95aa5e0
Alexander <alexander.zaycev@mail.ru>
2020-01-12 16:48:47 +0300

    Adding Columns and Indexes

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,11 @@ ActiveRecord::Schema.define(version: 2020_01_10_134215) do
     t.string "name"
     t.boolean "real"
     t.float "weight"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_items_on_name"
+    t.index ["price"], name: "index_items_on_price"
   end
 
 end
