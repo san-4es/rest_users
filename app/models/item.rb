9adbaeafa84ea8d9c125baac94686fae47358f9d
Alexander <alexander.zaycev@mail.ru>
2020-01-13 23:13:11 +0300

    Callbacks in models AR

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, :description, presence: true
+
+  after_initialize { puts "initialized" }
+  after_save       { puts "saved" }
+  after_create    { puts "created" }
+  after_update    { puts "updated" }
+  after_destroy   { puts "destroyed" }
+
 end
